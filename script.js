463b52ad344e02fe7d6846e439f118b8dda96ef2
Phumipat Thirawat <phumipat_thirawat@elearning.cmu.ac.th>
2021-05-28 16:46:55 +0700

    Revert "Commit5"
    This reverts commit 93c923282f662c965f95d64c80d1e40ae5c67de4.

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 document.getElementById('ok').addEventListener('click', function (e) {
-    alert('ok')
-    console.log(e)
-}
-)
-document.getElementById('cancel').addEventListener('click', function (e) {
-    alert('cancel')
-    console.log(e)
-}
-)
-document.getElementById('no').addEventListener('click', function (e) {
-    alert('no')
-    console.log(e)
+    alert('ok is click')
 }
 )
