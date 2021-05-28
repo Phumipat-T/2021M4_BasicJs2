56518ccdbf4dc02c936299976146c30b0d2f5180
Phumipat Thirawat <phumipat_thirawat@elearning.cmu.ac.th>
2021-05-28 17:23:05 +0700

    Commit6
    mouseover and alert done.

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 document.getElementById('ok').addEventListener('click', function (e) {
-    alert('ok')
+    alert('ok clicked')
+}
+)
+
+document.getElementById('cancel').addEventListener('mouseover', function (e) {
     console.log(e)
 }
 )
+
 document.getElementById('cancel').addEventListener('click', function (e) {
-    alert('cancel')
-    console.log(e)
+    alert('cancel clicked')
 }
 )
+
 document.getElementById('no').addEventListener('click', function (e) {
-    alert('no')
-    console.log(e)
+    alert('no clicked')
 }
 )
+
+
